3c22b929eed49c7e2ec544fc2b2ff36200f63b8b
akashyap25 <anuragkashyap026@gmail.com>
2023-07-15 00:32:33 +0530

    completed work section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -8,10 +8,10 @@ function About() {
    
 
   const abouts= [
-    {title:'Full Stack Development', Description:"I am a good full stack developer", imgUrl: images.about01},
-    {title:'Backend Development', Description:"I am a good backend developer", imgUrl: images.about04},
-    {title:'MERN Stack', Description:"I am a MERN stack developer", imgUrl: images.about02},
-    {title:'Problem Solving', Description:"I am a good problem solver", imgUrl:images.about03}
+    {title:'Full Stack Developer', Description:"As a full-stack developer, I specialize in using the MERN stack (MongoDB, Express.js, React.js, and Node.js) to build robust and scalable web applications. With MERN, I can seamlessly handle data storage, server-side logic, and dynamic front-end interfaces, ensuring a smooth user experience.", imgUrl: images.about01},
+    {title:'Backend Developer', Description:"As a backend developer, I specialize in designing and implementing robust server-side solutions to power web applications. With expertise in programming languages such as Node.js and frameworks like Express.js, I create efficient APIs, handle data storage and retrieval, and ensure seamless integration between the frontend and backend systems.", imgUrl: images.about04},
+    {title:'Database manager', Description:"I possess expertise in SQL (MySQL, PostgreSQL) and NoSQL (MongoDB) databases, excelling in designing, optimizing, and managing databases for efficient data storage, retrieval, and scalability. With a focus on maintaining data integrity and performance, I ensure seamless database operations and provide reliable solutions for diverse business needs.", imgUrl: images.about02},
+    {title:'Problem Solver', Description:"As a problem solver on LeetCode and Codeforces, I excel in utilizing data structures and algorithms (DSA) to efficiently solve challenging coding problems. With a strong understanding of DSA concepts and optimization techniques, I consistently strive to improve my problem solving skills and stay updated with the latest algorithmic trends.", imgUrl:images.about03}
   ]
 
   return (
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,19 +1,142 @@
-import React, {useState, useEffect} from 'react'
-import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
+
 import { AppWrap, MotionWrap } from '../../wrapper';
-import "./Work.scss";
+import './Work.scss';
 
-function Work() {
+const Work = () => {
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  useEffect(() => {
+ 
+    const work_array = [
+      {
+        imgUrl: 'https://raw.githubusercontent.com/akashyap25/We-Care/master/services.png',
+        projectLink: 'https://github.com/akashyap25/We-Care',
+        codeLink: 'https://github.com/akashyap25/We-Care',
+        title: 'We Care',
+        description:
+          'Developed a one-stop medical platform using ReactJS, ExpressJS, and MongoDB. Integrated APIs, enabling verified doctor’s appointments, bed booking, blood donors information, ambulance booking, vaccine availability, and AR-based health education.',
+        tags: ['We Care'],
+      },
+      {
+        imgUrl: 'https://shorturl.at/rtFU5',
+        projectLink: 'https://github.com/akashyap25/chat-it',
+        codeLink: 'https://github.com/akashyap25/chat-it',
+        title: 'Chat it',
+        description:
+          'Developed a user-friendly real-time chatting app with secure user login functionality and robust database integration, ensuring seamless communication and data management.',
+        tags: ['Chat it'],
+      },
+      {
+        imgUrl: 'https://shorturl.at/buCU2',
+        projectLink: 'https://akashyap25.github.io/Web/',
+        codeLink: 'https://github.com/akashyap25/Web',
+        title: 'Currency Converter',
+        description:
+          'Successfully created a currency converter website, offering accurate exchange rates for 57 currencies, providing users with convenient and reliable currency conversion capabilities.',
+        tags: ['Currency Converter',],
+      },
+    ];
+    
+
+    setWorks(work_array);
+    setFilterWork(work_array);
+  }, []);
+
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+
   return (
-    <div>
-       <h2 className="head-text ">
-        My  
-        <span> Projects</span>
-        <br/>
-      </h2>
-    </div>
-  )
-}
+    <>
+      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-export default Work
+      <div className="app__work-filter">
+        {['We Care', 'Chat it', 'Currency Converter', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={work.imgUrl} alt={work.title} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg',
+);
